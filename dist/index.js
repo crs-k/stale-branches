e844df2374d3caf58a241c04b90412f1af8531f8
Chris <26232872+crs-k@users.noreply.github.com>
2022-01-21 21:06:03 -0500

    round

--- dist/index.js
+++ dist/index.js
@@ -204,25 +204,25 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getnSeconds = exports.getMinutes = exports.getHours = exports.getDays = void 0;
 function getDays(date1, date2) {
     const diffMs = Math.abs(date2 - date1);
-    const days = diffMs / (1000 * 60 * 60 * 24);
+    const days = Math.round(diffMs / (1000 * 60 * 60 * 24));
     return days.toString();
 }
 exports.getDays = getDays;
 function getHours(date1, date2) {
     const diffMs = Math.abs(date2 - date1);
-    const hours = diffMs / (1000 * 60 * 60);
+    const hours = Math.round(diffMs / (1000 * 60 * 60));
     return hours.toString();
 }
 exports.getHours = getHours;
 function getMinutes(date1, date2) {
     const diffMs = Math.abs(date2 - date1);
-    const minutes = diffMs / (1000 * 60);
+    const minutes = Math.round(diffMs / (1000 * 60));
     return minutes.toString();
 }
 exports.getMinutes = getMinutes;
 function getnSeconds(date1, date2) {
     const diffMs = Math.abs(date2 - date1);
-    const seconds = diffMs / 1000;
+    const seconds = Math.round(diffMs / 1000);
     return seconds.toString();
 }
 exports.getnSeconds = getnSeconds;
--- src/functions/get-time.ts
+++ src/functions/get-time.ts
@@ -1,23 +1,23 @@
 export function getDays(date1, date2): string {
   const diffMs = Math.abs(date2 - date1)
-  const days = diffMs / (1000 * 60 * 60 * 24)
+  const days = Math.round(diffMs / (1000 * 60 * 60 * 24))
   return days.toString()
 }
 
 export function getHours(date1, date2): string {
   const diffMs = Math.abs(date2 - date1)
-  const hours = diffMs / (1000 * 60 * 60)
+  const hours = Math.round(diffMs / (1000 * 60 * 60))
   return hours.toString()
 }
 
 export function getMinutes(date1, date2): string {
   const diffMs = Math.abs(date2 - date1)
-  const minutes = diffMs / (1000 * 60)
+  const minutes = Math.round(diffMs / (1000 * 60))
   return minutes.toString()
 }
 
 export function getnSeconds(date1, date2): string {
   const diffMs = Math.abs(date2 - date1)
-  const seconds = diffMs / 1000
+  const seconds = Math.round(diffMs / 1000)
   return seconds.toString()
 }
