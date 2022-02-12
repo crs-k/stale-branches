7d4b4dbeb4de6d189cc11908149545df2bbf0759
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-11 20:05:29 -0500

    Changed warning to notice

--- src/functions/delete-branch.ts
+++ src/functions/delete-branch.ts
@@ -3,7 +3,6 @@ import * as core from '@actions/core'
 import {github, owner, repo} from './get-context'
 
 export async function deleteBranch(name: string): Promise<number> {
-  core.info('Retrieving branch information...')
   let confirm: number
   const refAppend = 'heads/'
   const refFull = refAppend.concat(name)
@@ -18,7 +17,7 @@ export async function deleteBranch(name: string): Promise<number> {
     confirm = response.status
 
     assert.ok(response, 'name cannot be empty')
-    core.warning(`Branch: ${refFull} has been deleted.`)
+    core.notice(`Branch: ${refFull} has been deleted.`)
   } catch (err) {
     if (err instanceof Error) core.error(`Failed to delete branch ${refFull}:  ${err.message}`)
     confirm = 500
