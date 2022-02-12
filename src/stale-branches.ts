1eb5fd39f2f2448a04f026bda356ac10c5a5100a
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-11 21:30:28 -0500

    color test

--- src/stale-branches.ts
+++ src/stale-branches.ts
@@ -84,7 +84,7 @@ export async function run(): Promise<void> {
       }
     }
     core.notice(`Stale Branches:  ${JSON.stringify(outputStales)}`)
-    core.notice(`Deleted Branches:  ${JSON.stringify(outputDeletes)}`)
+    core.notice(`\u001b[31;46mDeleted Branches:  ${JSON.stringify(outputDeletes)}`)
     core.setOutput('stale-branches', JSON.stringify(outputStales))
     core.setOutput('deleted-branches', JSON.stringify(outputDeletes))
   } catch (error) {
