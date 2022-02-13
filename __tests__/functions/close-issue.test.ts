c77065d2e7e986446ee701c971d60532c3d5b0e0
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 19:36:00 -0500

    increased coverage

--- __tests__/functions/close-issue.test.ts
+++ __tests__/functions/close-issue.test.ts
@@ -1,33 +1,45 @@
 jest.mock('@actions/core')
 jest.mock('@actions/github')
+jest.mock('assert')
+jest.mock('../../src/functions/get-context')
 
 const core = require('@actions/core')
+const assert = require('assert')
 import {closeIssue} from '../../src/functions/close-issue'
 import {github} from '../../src/functions/get-context'
 
-jest.mock('../../src/functions/get-context')
 let issueNumber = 1
+
 describe('Close Issue Function', () => {
-  beforeEach(() => {})
   test('closeIssue endpoint is called', async () => {
     await closeIssue(issueNumber)
-
-    expect(github.rest.issues.update).toHaveBeenCalled()
+    expect(github.rest.issues.update).toHaveBeenCalledWith({
+      issue_number: 1,
+      owner: 'owner',
+      repo: 'repo',
+      state: 'closed'
+    })
+    expect(github.rest.issues.update).toHaveReturnedWith({
+      data: {issue_number: 1, owner: 'owner', repo: 'repo', state: 'closed'}
+    })
   })
 
   test('Infos are set', async () => {
     core.info = jest.fn()
     await closeIssue(issueNumber)
-
-    expect(core.info).toHaveBeenCalled()
+    expect(core.info).toHaveBeenCalledWith(`Issue #1's state was changed to closed.`)
   })
 
   test('Action fails elegantly', async () => {
     core.info = jest.fn()
+    assert.ok = jest.fn()
+    assert.ok.mockImplementation(() => {
+      throw new Error('State cannot be empty')
+    })
 
     await closeIssue(issueNumber)
     expect(core.info).toHaveBeenCalledWith(
-      `No existing issue returned for issue number: 1. Description: Cannot read properties of undefined (reading 'data')`
+      `No existing issue returned for issue number: 1. Description: State cannot be empty`
     )
   })
 })
