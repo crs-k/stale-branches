d943850a90e07a6f3c84fffa3c47b18fee97535d
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 23:33:29 -0500

    removed or

--- src/functions/update-issue.ts
+++ src/functions/update-issue.ts
@@ -27,8 +27,8 @@ export async function updateIssue(
         ]
       })
 
-      createdAt = issueResponse.data.created_at || ''
-      commentUrl = issueResponse.data.html_url || ''
+      createdAt = issueResponse.data.created_at
+      commentUrl = issueResponse.data.html_url
       assert.ok(createdAt, 'Created At cannot be empty')
       core.info(`Issue #${issueNumber}: comment was created at ${createdAt}. ${commentUrl}`)
     } catch (err) {
