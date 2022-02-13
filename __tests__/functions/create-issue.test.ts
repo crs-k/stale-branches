7d64e471d13b37d30e5a0a39cc0a77137d8dd8cb
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 20:58:26 -0500

    increased coverage

--- __tests__/functions/create-issue.test.ts
+++ __tests__/functions/create-issue.test.ts
@@ -1,27 +1,35 @@
 jest.mock('@actions/core')
 jest.mock('@actions/github')
+jest.mock('assert')
+jest.mock('../../src/functions/get-context')
 
 const core = require('@actions/core')
+const assert = require('assert')
 import {createIssue} from '../../src/functions/create-issue'
 import {github} from '../../src/functions/get-context'
 
-jest.mock('../../src/functions/get-context')
 let branchName = 'test'
 let commitAge = 1
+
 describe('Create Issue Function', () => {
-  beforeEach(() => {})
   test('createIssue endpoint is called', async () => {
     await createIssue(branchName, commitAge)
 
     expect(github.rest.issues.create).toHaveBeenCalled()
+    expect(github.rest.issues.create).toHaveReturnedWith({
+      data: {id: 1, owner: 'owner', repo: 'repo', state: 'closed'}
+    })
   })
-
   test('Action fails elegantly', async () => {
     core.setFailed = jest.fn()
+    assert.ok = jest.fn()
+    assert.ok.mockImplementation(() => {
+      throw new Error('Issue ID cannot be empty')
+    })
 
     await createIssue(branchName, commitAge)
     expect(core.setFailed).toHaveBeenCalledWith(
-      `Failed to create issue for test. Error: Cannot read properties of undefined (reading 'data')`
+      `Failed to create issue for test. Error: Issue ID cannot be empty`
     )
   })
 })
