c9d66d398b07600d878b55fcd9fd75173a33b595
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 23:33:38 -0500

    increased coverage

--- __tests__/functions/update-issue.test.ts
+++ __tests__/functions/update-issue.test.ts
@@ -1,32 +1,36 @@
 jest.mock('@actions/core')
 jest.mock('@actions/github')
+jest.mock('assert')
+jest.mock('../../src/functions/get-context')
 
 const core = require('@actions/core')
+const assert = require('assert')
 import {updateIssue} from '../../src/functions/update-issue'
 import {github} from '../../src/functions/get-context'
-jest.mock('../../src/functions/get-context')
+import  * as context  from '../../src/functions/get-context'
 
 let issueNumber = 20
 let branchName = 'test'
 let commitAge = 100
 
 describe('Get Commits Function', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
 
-  test('updateIssue endpoint is called with default inputs', async () => {
+  test('updateIssue endpoint is called', async () => {
+    Object.defineProperty(context,'commentUpdates',{value: true})
     await updateIssue(issueNumber, branchName, commitAge)
-
-    expect(github.rest.issues.createComment).toHaveBeenCalledTimes(0)
+    expect(github.rest.issues.createComment).toHaveBeenCalled()
   })
 
-  /*   test('Action fails elegantly', async () => {
+  test('Action fails elegantly', async () => {
     core.info = jest.fn()
-    
-    await updateIssue(issueNumber,branchName,commitAge)
-  
-    expect(github.rest.issues.createComment).toHaveBeenCalled()
-    expect(core.info).toHaveBeenCalledWith(`Failed to locate issues.`)
-  }) */
+    assert.ok = jest.fn()
+    assert.ok.mockImplementation(() => {
+      throw new Error('Created At cannot be empty')
+    })
+
+    await updateIssue(issueNumber, branchName, commitAge)
+    expect(core.info).toHaveBeenCalledWith(
+      `No existing issue returned for issue number: 20. Error: Created At cannot be empty`
+    )
+  })
 })
