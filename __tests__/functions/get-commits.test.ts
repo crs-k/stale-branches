4f30c8a325775c9cbe341f3b788256e53c21bda8
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 22:09:44 -0500

    added coverage

--- __tests__/functions/get-commits.test.ts
+++ __tests__/functions/get-commits.test.ts
@@ -1,11 +1,13 @@
 jest.mock('@actions/core')
 jest.mock('@actions/github')
+jest.mock('assert')
+jest.mock('../../src/functions/get-context')
 
 const core = require('@actions/core')
+const assert = require('assert')
 import {getRecentCommitDate} from '../../src/functions/get-commits'
 import {github} from '../../src/functions/get-context'
 
-jest.mock('../../src/functions/get-context')
 let sha = '123'
 describe('Get Commits Function', () => {
   beforeEach(() => {
@@ -25,12 +27,14 @@ describe('Get Commits Function', () => {
 
   test('Action fails elegantly', async () => {
     core.setFailed = jest.fn()
+    assert.ok = jest.fn()
+    assert.ok.mockImplementation(() => {
+      throw new Error('Date cannot be empty.')
+    })
 
     await getRecentCommitDate(sha)
-
-    expect(github.rest.repos.getCommit).toHaveBeenCalled()
     expect(core.setFailed).toHaveBeenCalledWith(
-      `Failed to retrieve commit for repo. Error: Cannot read properties of undefined (reading 'data')`
+      `Failed to retrieve commit for repo. Error: Date cannot be empty.`
     )
   })
 })
