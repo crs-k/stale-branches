459106e5ba713e5c30142f21ea24d17c15f04d47
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-12 23:31:11 -0500

    added export

--- __mocks__/@actions/github.ts
+++ __mocks__/@actions/github.ts
@@ -8,6 +8,9 @@ export const context = {
 export const daysBeforeStale: number = 120
 export const daysBeforeDelete: number = 180
 export const commentUpdates: boolean = true
+export const maxIssues: number = 20
+export const repoToken: string = '20'
+
 let update = jest.fn().mockReturnValue({
   data: {issue_number: 1, owner: 'owner', repo: 'repo', state: 'closed'}
 })
@@ -28,14 +31,22 @@ let getCommit = jest.fn().mockReturnValue({
   data: {id: 1, commit: {author: {date: 'January 25, 2006'}}, repo: 'repo'}
 })
 
+let listForRepo = jest.fn().mockReturnValue({
+  data: {id: 1, owner: 'owner', repo: 'repo'}
+})
+
+let createComment = jest.fn().mockReturnValue({
+  data: {id: 1, owner: 'owner', repo: 'repo'}
+})
+
 const github = {
   rest: {
     git: {deleteRef},
     issues: {
       update,
       create,
-      listForRepo: jest.fn(),
-      createComment: jest.fn()
+      listForRepo,
+      createComment
     },
     repos: {
       get: jest.fn(),
