70be3658c537faa679bdbe7d8e9419e5a2e7d0c4
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-16 21:20:59 -0500

    added switch for lastCommitter

--- src/functions/create-issue.ts
+++ src/functions/create-issue.ts
@@ -1,16 +1,27 @@
 import * as assert from 'assert'
 import * as core from '@actions/core'
-import {daysBeforeDelete, github, owner, repo} from './get-context'
+import {daysBeforeDelete, github, owner, repo, tagLastComitter} from './get-context'
 
-export async function createIssue(branch: string, commitAge: number): Promise<number> {
+
+
+export async function createIssue(branch: string, commitAge: number, lastCommitter: string): Promise<number> {
   let issueId: number
+  let bodyString: string
   const daysUntilDelete = Math.max(0, daysBeforeDelete - commitAge)
+  
+  switch (tagLastComitter) {
+    case true:
+      bodyString =`${lastCommitter}, ${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+    case false:
+      bodyString=`${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+  }
+
   try {
     const issueResponse = await github.rest.issues.create({
       owner,
       repo,
       title: `[${branch}] is STALE`,
-      body: `${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`,
+      body: bodyString,
       labels: [
         {
           name: 'stale branch 🗑️',
