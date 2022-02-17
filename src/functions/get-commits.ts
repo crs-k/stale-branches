afff3f624093a47de9f155505f5d942247bcaa39
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-16 20:48:14 -0500

    feature tag last committer

--- src/functions/get-commits.ts
+++ src/functions/get-commits.ts
@@ -4,6 +4,7 @@ import {github, owner, repo} from './get-context'
 
 export async function getRecentCommitDate(sha: string): Promise<string> {
   let commitDate: string | undefined
+  let lastCommitter: string | undefined
   try {
     const branchResponse = await github.rest.repos.getCommit({
       owner,
@@ -12,8 +13,10 @@ export async function getRecentCommitDate(sha: string): Promise<string> {
       per_page: 1,
       page: 1
     })
-    commitDate = branchResponse.data.commit.author!.date
+    commitDate = branchResponse.data.commit.committer!.date
+    lastCommitter = branchResponse.data.committer!.login
     assert.ok(commitDate, 'Date cannot be empty.')
+    assert.ok(lastCommitter, 'Committer cannot be empty.')
   } catch (err) {
     if (err instanceof Error)
       core.setFailed(`Failed to retrieve commit for ${repo}. Error: ${err.message}`)
