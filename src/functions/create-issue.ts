6880c748a19539a4252579f4b84e96cff8521ccd
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-16 21:40:30 -0500

    formatting

--- src/functions/create-issue.ts
+++ src/functions/create-issue.ts
@@ -2,18 +2,22 @@ import * as assert from 'assert'
 import * as core from '@actions/core'
 import {daysBeforeDelete, github, owner, repo, tagLastComitter} from './get-context'
 
-
-
-export async function createIssue(branch: string, commitAge: number, lastCommitter: string): Promise<number> {
+export async function createIssue(
+  branch: string,
+  commitAge: number,
+  lastCommitter: string
+): Promise<number> {
   let issueId: number
   let bodyString: string
   const daysUntilDelete = Math.max(0, daysBeforeDelete - commitAge)
-  
+
   switch (tagLastComitter) {
     case true:
-      bodyString =`${lastCommitter}, \r \r ${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+      bodyString = `${lastCommitter}, \r \r ${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+      break
     case false:
-      bodyString=`${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+      bodyString = `${branch} has had no activity for ${commitAge.toString()} days. \r \r This branch will be automatically deleted in ${daysUntilDelete.toString()} days.`
+      break
   }
 
   try {
