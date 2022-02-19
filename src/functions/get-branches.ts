f3c6fdb7db137a8eb649c7eba833db517b819887
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-18 21:43:03 -0500

    pagination update

--- src/functions/get-branches.ts
+++ src/functions/get-branches.ts
@@ -3,16 +3,37 @@ import * as core from '@actions/core'
 import {github, owner, repo} from './get-context'
 // eslint-disable-next-line import/named
 import {GetResponseTypeFromEndpointMethod} from '@octokit/types'
+import {
+  PaginateInterface,
+  PaginatingEndpoints,
+} from "@octokit/plugin-paginate-rest";
 
 type ListBranchesResponseDataType = GetResponseTypeFromEndpointMethod<
   typeof github.rest.repos.listBranches
 >
 
 export async function getBranches(): Promise<ListBranchesResponseDataType> {
-  let branches: ListBranchesResponseDataType
+  let branches = {} as ListBranchesResponseDataType
 
   try {
-    // Get info from the most recent release
+    
+    const parameters = {
+      owner: owner,
+      repo: repo,
+      per_page: 100,
+    }
+    
+    for await (const response of github.paginate.iterator(
+      github.rest.repos.listBranches,
+      parameters
+    )) {
+      // do whatever you want with each response, break out of the loop, etc.
+      branches = response
+      core.info(`${branches.data.length} branches found`)
+      assert.ok(response, 'Response cannot be empty.')
+    }
+
+/*     // Get info from the most recent release
     const response = await github.rest.repos.listBranches({
       owner,
       repo,
@@ -20,9 +41,9 @@ export async function getBranches(): Promise<ListBranchesResponseDataType> {
       per_page: 100,
       page: 1
     })
-    branches = response
+    branches = response */
 
-    assert.ok(response, 'Response cannot be empty.')
+    
   } catch (err) {
     if (err instanceof Error) {
       core.setFailed(`Failed to retrieve branches for ${repo}. Error: ${err.message}`)
