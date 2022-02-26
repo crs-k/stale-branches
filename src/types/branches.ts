25edb8a3ea6964bf5be49d64b8a187bc4ff62e96
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-26 12:53:04 -0500

    added description

--- src/types/branches.ts
+++ src/types/branches.ts
@@ -1,4 +1,15 @@
 export interface BranchResponse {
+  /**
+   * The Name of the Branch.
+   *
+   * Example: test-branch-19
+   */
   branchName: string
+
+  /**
+   * The SHA of a commit made to the Branch.
+   *
+   * Example: aef86c0cfec5f986ab3032af9010bbd6923b92ee
+   */
   commmitSha: string
 }
