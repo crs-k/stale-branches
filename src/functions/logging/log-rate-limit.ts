6ef6f6976812a88913a958c169264a8c4ebec7bc
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-28 21:57:41 -0500

    color check

--- src/functions/logging/log-rate-limit.ts
+++ src/functions/logging/log-rate-limit.ts
@@ -2,15 +2,15 @@ import {RateLimit} from '../../types/rate-limit'
 import styles from 'ansi-styles'
 
 export function logRateLimit(rateLimit: RateLimit): string {
-  let rateLimitColor = `Rate Limit Used: ${styles.greenBright.open}${rateLimit.used}%${styles.greenBright.close}, Rate Limit Remaining: ${styles.greenBright.open}${rateLimit.remaining}%${styles.greenBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
+  let rateLimitColor = `Rate Limit Used: ${styles.greenBright.open}${rateLimit.used}%${styles.greenBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
 
   // color output based on remaining rate limit %
-  if (rateLimit.used < 95) {
-    rateLimitColor = `Rate Limit Used: ${styles.redBright.open}${rateLimit.used}%${styles.redBright.close}, Rate Limit Remaining: ${styles.redBright.open}${rateLimit.remaining}%${styles.redBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
-  } else if (rateLimit.used > 85) {
-    rateLimitColor = `Rate Limit Used: ${styles.yellowBright.open}${rateLimit.used}%${styles.yellowBright.close}, Rate Limit Remaining: ${styles.yellowBright.open}${rateLimit.remaining}%${styles.yellowBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
+  if (rateLimit.used > 95) {
+    rateLimitColor = `Rate Limit Used: ${styles.redBright.open}${rateLimit.used}%${styles.redBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
   } else if (rateLimit.used < 85) {
-    rateLimitColor = `Rate Limit Used: ${styles.greenBright.open}${rateLimit.used}%${styles.greenBright.close}, Rate Limit Remaining: ${styles.greenBright.open}${rateLimit.remaining}%${styles.greenBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
+    rateLimitColor = `Rate Limit Used: ${styles.yellowBright.open}${rateLimit.used}%${styles.yellowBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
+  } else if (rateLimit.used < 0) {
+    rateLimitColor = `Rate Limit Used: ${styles.greenBright.open}${rateLimit.used}%${styles.greenBright.close}, Resets in ${styles.magentaBright.open}${rateLimit.remaining}${styles.magentaBright.close} minutes.`
   }
 
   return rateLimitColor
