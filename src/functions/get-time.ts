4555febf534b90a8f3ba50cd75a6418538451d8e
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-03 21:37:08 -0500

    declared type for inputs

--- src/functions/get-time.ts
+++ src/functions/get-time.ts
@@ -1,10 +1,10 @@
-export function getDays(date1, date2): number {
+export function getDays(date1: number, date2: number): number {
   const diffMs = Math.abs(date2 - date1)
   const days = Math.round(diffMs / (1000 * 60 * 60 * 24))
   return days
 }
 
-export function getMinutes(date1, date2): number {
+export function getMinutes(date1: number, date2: number): number {
   const diffMs = Math.abs(date2 - date1)
   const minutes = Math.round(diffMs / (1000 * 60))
   return minutes
