6128f5ce24a94b87a772bc3a1dcd0cf0703cd7bc
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-03 21:53:37 -0500

    added issue title function

--- src/functions/logging/log-new-issue.ts
+++ src/functions/logging/log-new-issue.ts
@@ -1,7 +1,9 @@
+import {createIssueTitle} from '../../utils/create-issues-title'
 import styles from 'ansi-styles'
 
 export function logNewIssue(branchName): string {
-  const newIssue = `${styles.bold.open}New issue created:${styles.bold.close} ${styles.magentaBright.open}[${branchName}] is STALE${styles.magentaBright.close}.`
+  const issueTitleString = createIssueTitle(branchName)
+  const newIssue = `${styles.bold.open}New issue created:${styles.bold.close} ${styles.magentaBright.open}${issueTitleString}${styles.magentaBright.close}.`
 
   return newIssue
 }
