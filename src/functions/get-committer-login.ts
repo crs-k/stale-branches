1ef591196c3e9572c88f47e4c1857ee0931ac310
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-03 21:36:58 -0500

    renamed constant

--- src/functions/get-committer-login.ts
+++ src/functions/get-committer-login.ts
@@ -5,14 +5,14 @@ import {github, owner, repo} from './get-context'
 export async function getRecentCommitLogin(sha: string): Promise<string> {
   let lastCommitter: string | undefined
   try {
-    const branchResponse = await github.rest.repos.getCommit({
+    const commitResponse = await github.rest.repos.getCommit({
       owner,
       repo,
       ref: sha,
       per_page: 1,
       page: 1
     })
-    lastCommitter = branchResponse.data.committer!.login
+    lastCommitter = commitResponse.data.committer!.login
     assert.ok(lastCommitter, 'Committer cannot be empty.')
   } catch (err) {
     if (err instanceof Error) {
