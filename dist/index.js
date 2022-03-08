7948991655818a4aaa4eec3abbf7f195d0c89183
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-07 22:05:17 -0500

    output update

--- dist/index.js
+++ dist/index.js
@@ -1038,19 +1038,19 @@ const ansi_styles_1 = __importDefault(__nccwpck_require__(2068));
 function logCompareBranches(branchComparison, base, head) {
     //const compareBranches1 = `${styles.bold.open}[${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close}] ${styles.blueBright.open}branches found${styles.blueBright.close}.${styles.bold.close}`
     let compareBranches;
-    compareBranches = `${ansi_styles_1.default.bold.open}${head} is ${branchComparison.branchStatus} ${base} by aheadby: ${branchComparison.aheadBy} behindby: ${branchComparison.behindBy} with ${branchComparison.totalCommits} total commits${ansi_styles_1.default.bold.close}`;
+    compareBranches = `${ansi_styles_1.default.bold.open}${head} has a status of [${branchComparison.branchStatus}] in comparison to ${base}. ${head} is ahead by ${branchComparison.aheadBy} commits and behind by ${branchComparison.behindBy} commits.${ansi_styles_1.default.bold.close}`;
     switch (branchComparison.branchStatus) {
         case 'diverged':
-            compareBranches = `${ansi_styles_1.default.bold.open}${head} has diverged from ${base}, and is ahead by ${branchComparison.aheadBy} commits and behind by ${branchComparison.behindBy}.${ansi_styles_1.default.bold.close}`;
+            compareBranches = `${ansi_styles_1.default.bold.open}${head} has ${ansi_styles_1.default.magenta.open}diverged${ansi_styles_1.default.magenta.close} from ${base}, and is ahead by ${ansi_styles_1.default.magenta.open}${branchComparison.aheadBy}${ansi_styles_1.default.magenta.close} commits and behind by ${ansi_styles_1.default.magenta.open}${branchComparison.behindBy}${ansi_styles_1.default.magenta.close} commits.${ansi_styles_1.default.bold.close}`;
             break;
         case 'ahead':
-            compareBranches = `${ansi_styles_1.default.bold.open}${head} is ahead of ${base} by ${branchComparison.aheadBy} commits.${ansi_styles_1.default.bold.close}`;
+            compareBranches = `${ansi_styles_1.default.bold.open}${head} is ${ansi_styles_1.default.yellow.open}ahead${ansi_styles_1.default.yellow.close} of ${base} by ${ansi_styles_1.default.magenta.open}${branchComparison.aheadBy}${ansi_styles_1.default.magenta.close} commits.${ansi_styles_1.default.bold.close}`;
             break;
         case 'behind':
-            compareBranches = `${ansi_styles_1.default.bold.open}${head} is behind ${base} by ${branchComparison.behindBy} commits.${ansi_styles_1.default.bold.close}`;
+            compareBranches = `${ansi_styles_1.default.bold.open}${head} is ${ansi_styles_1.default.red.open}behind${ansi_styles_1.default.red.close} ${base} by ${ansi_styles_1.default.magenta.open}${branchComparison.behindBy}${ansi_styles_1.default.magenta.close} commits.${ansi_styles_1.default.bold.close}`;
             break;
         case 'identical':
-            compareBranches = `${ansi_styles_1.default.bold.open}${head} is identical to ${base}.${ansi_styles_1.default.bold.close}`;
+            compareBranches = `${ansi_styles_1.default.bold.open}${head} is ${ansi_styles_1.default.green.open}identical${ansi_styles_1.default.green.close} to ${base}.${ansi_styles_1.default.bold.close}`;
             break;
     }
     return compareBranches;
--- src/functions/logging/log-compare-branches.ts
+++ src/functions/logging/log-compare-branches.ts
@@ -4,19 +4,19 @@ import styles from 'ansi-styles'
 export function logCompareBranches(branchComparison: BranchComparison, base: string, head: string): string {
   //const compareBranches1 = `${styles.bold.open}[${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close}] ${styles.blueBright.open}branches found${styles.blueBright.close}.${styles.bold.close}`
   let compareBranches: string
-  compareBranches = `${styles.bold.open}${head} is ${branchComparison.branchStatus} ${base} by aheadby: ${branchComparison.aheadBy} behindby: ${branchComparison.behindBy} with ${branchComparison.totalCommits} total commits${styles.bold.close}`
+  compareBranches = `${styles.bold.open}${head} has a status of [${branchComparison.branchStatus}] in comparison to ${base}. ${head} is ahead by ${branchComparison.aheadBy} commits and behind by ${branchComparison.behindBy} commits.${styles.bold.close}`
   switch (branchComparison.branchStatus) {
     case 'diverged':
-      compareBranches = `${styles.bold.open}${head} has diverged from ${base}, and is ahead by ${branchComparison.aheadBy} commits and behind by ${branchComparison.behindBy}.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} has ${styles.magenta.open}diverged${styles.magenta.close} from ${base}, and is ahead by ${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close} commits and behind by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'ahead':
-      compareBranches = `${styles.bold.open}${head} is ahead of ${base} by ${branchComparison.aheadBy} commits.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} is ${styles.yellow.open}ahead${styles.yellow.close} of ${base} by ${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'behind':
-      compareBranches = `${styles.bold.open}${head} is behind ${base} by ${branchComparison.behindBy} commits.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} is ${styles.red.open}behind${styles.red.close} ${base} by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'identical':
-      compareBranches = `${styles.bold.open}${head} is identical to ${base}.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} is ${styles.green.open}identical${styles.green.close} to ${base}.${styles.bold.close}`
       break
   }
   return compareBranches
