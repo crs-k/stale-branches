c0d0cb320f2a1c81799be7f7353028e5d739dff0
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-07 22:07:19 -0500

    output update

--- src/functions/logging/log-compare-branches.ts
+++ src/functions/logging/log-compare-branches.ts
@@ -7,13 +7,13 @@ export function logCompareBranches(branchComparison: BranchComparison, base: str
   compareBranches = `${styles.bold.open}${head} has a status of [${branchComparison.branchStatus}] in comparison to ${base}. ${head} is ahead by ${branchComparison.aheadBy} commits and behind by ${branchComparison.behindBy} commits.${styles.bold.close}`
   switch (branchComparison.branchStatus) {
     case 'diverged':
-      compareBranches = `${styles.bold.open}${head} has ${styles.magenta.open}diverged${styles.magenta.close} from ${base}, and is ahead by ${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close} commits and behind by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} has ${styles.red.open}diverged${styles.red.close} from ${base}, and is ahead by ${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close} commits and behind by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'ahead':
       compareBranches = `${styles.bold.open}${head} is ${styles.yellow.open}ahead${styles.yellow.close} of ${base} by ${styles.magenta.open}${branchComparison.aheadBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'behind':
-      compareBranches = `${styles.bold.open}${head} is ${styles.red.open}behind${styles.red.close} ${base} by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
+      compareBranches = `${styles.bold.open}${head} is ${styles.yellow.open}behind${styles.yellow.close} ${base} by ${styles.magenta.open}${branchComparison.behindBy}${styles.magenta.close} commits.${styles.bold.close}`
       break
     case 'identical':
       compareBranches = `${styles.bold.open}${head} is ${styles.green.open}identical${styles.green.close} to ${base}.${styles.bold.close}`
