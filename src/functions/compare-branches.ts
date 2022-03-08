60ee1fcf3448998fc953f8a7b4bd35bf72f5471c
Chris <26232872+crs-k@users.noreply.github.com>
2022-03-07 21:18:18 -0500

    initial commit

---
+++ src/functions/compare-branches.ts
@@ -0,0 +1,36 @@
+import * as assert from 'assert'
+import * as core from '@actions/core'
+import {github, owner, repo} from './get-context'
+import {BranchComparison} from '../types/branch-comparison'
+import { getDefaultBranch } from './get-default-branch'
+import {logGetBranches} from './logging/log-get-branches'
+
+export async function compareBranches(head: string): Promise<BranchComparison> {
+  let branchComparison= {} as unknown as BranchComparison
+  const base = await getDefaultBranch()
+  const basehead = `${base}${head}`
+  try {
+
+    const branchComparisonResponse = await github.rest.repos.compareCommitsWithBasehead({
+        owner,
+        repo,
+        basehead
+      })
+
+    branchComparison.aheadBy = branchComparisonResponse.data.ahead_by
+    branchComparison.behindBy = branchComparisonResponse.data.behind_by
+    branchComparison.branchStatus = branchComparisonResponse.data.status
+    branchComparison.totalCommits = branchComparisonResponse.data.total_commits
+
+    assert.ok(branchComparison.branchStatus, 'Branch Comparison Status cannot be empty.')
+    core.info(logGetBranches(branchComparison.branchStatus))
+} catch (err) {
+    if (err instanceof Error) {
+      core.info(`Failed to retrieve branch comparison data. Error: ${err.message}`)
+    } else {
+      core.info(`Failed to retrieve branch comparison data.`)
+    }
+  }
+
+  return branchComparison
+}
---
+++ src/types/branch-comparison.ts
@@ -0,0 +1,30 @@
+export interface BranchComparison {
+    /**
+     * The comparative status of the head branch to the base branch.
+     *
+     * Example: "diverged" | "ahead" | "behind" | "identical"
+     */
+    branchStatus: string
+  
+    /**
+     * How many commits the head branch is ahead of the base branch.
+     *
+     * Example: 1
+     */
+    aheadBy: number
+
+    /**
+     * How many commits the head branch is behind the base branch.
+     *
+     * Example: 2
+     */
+    behindBy: number
+
+    /**
+     * The total number of commits to the base branch
+     *
+     * Example: 1
+     */
+    totalCommits: number
+  }
+  
