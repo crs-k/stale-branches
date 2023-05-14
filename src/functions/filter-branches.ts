f07eb90b7a62d469c104a996f33ed52749ed7392
Phil Ohnemus <1946398+pohnemus@users.noreply.github.com>
2023-05-14 21:18:50 +0000

    Add some docs

--- src/functions/filter-branches.ts
+++ src/functions/filter-branches.ts
@@ -6,7 +6,7 @@ import {logFilterBranches} from './logging/log-filter-branches'
  * Retrieves filtered list of branches
  *
  * @param {BranchResponse[]} branches A list of branches
- * @param {string} ignoreBranchesRegex A string that indicates which branches to ignore
+ * @param {string} ignoreBranchesRegex A RegExp string that indicates which branches to ignore
  * @returns {BranchResponse[]} A filtered list of branches that do not meet the passed in RegEx @see {@link BranchResponse}
  */
 export async function filterBranches(branches, ignoreBranchesRegex): Promise<BranchResponse[]> {
