7ab36abec66f5fd282e811ed7070ba30ea428815
ck <clkerins@gmail.com>
2023-05-16 08:32:52 -0400

    build update

--- dist/index.js
+++ dist/index.js
@@ -494,69 +494,6 @@ exports.deleteBranch = deleteBranch;
 
 /***/ }),
 
-/***/ 7168:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.filterBranches = void 0;
-const core = __importStar(__nccwpck_require__(2186));
-const log_filter_branches_1 = __nccwpck_require__(46);
-/**
- * Retrieves filtered list of branches
- *
- * @param {BranchResponse[]} branches A list of branches
- * @param {string} branchesFilterRegex A RegExp string that indicates which branches to include
- * @returns {BranchResponse[]} A filtered list of branches that meet the passed in RegEx @see {@link BranchResponse}
- */
-function filterBranches(branches, branchesFilterRegex) {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (branchesFilterRegex !== null && branchesFilterRegex !== '') {
-            const pattern = new RegExp(`${branchesFilterRegex}`);
-            core.info((0, log_filter_branches_1.logFilterBranches)(branches.length));
-            return branches.filter(branch => pattern.test(branch.branchName));
-        }
-        return branches;
-    });
-}
-exports.filterBranches = filterBranches;
-
-
-/***/ }),
-
 /***/ 6204:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -1655,6 +1592,69 @@ exports.createIssueTitleString = createIssueTitleString;
 
 /***/ }),
 
+/***/ 6261:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.filterBranches = void 0;
+const core = __importStar(__nccwpck_require__(2186));
+const log_filter_branches_1 = __nccwpck_require__(46);
+/**
+ * Retrieves filtered list of branches
+ *
+ * @param {BranchResponse[]} branches A list of branches
+ * @param {string} branchesFilterRegex A RegExp string that indicates which branches to include
+ * @returns {BranchResponse[]} A filtered list of branches that meet the passed in RegEx @see {@link BranchResponse}
+ */
+function filterBranches(branches, branchesFilterRegex) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (branchesFilterRegex !== null && branchesFilterRegex !== '') {
+            const pattern = new RegExp(`${branchesFilterRegex}`);
+            core.info((0, log_filter_branches_1.logFilterBranches)(branches.length));
+            return branches.filter(branch => pattern.test(branch.branchName));
+        }
+        return branches;
+    });
+}
+exports.filterBranches = filterBranches;
+
+
+/***/ }),
+
 /***/ 2643:
 /***/ ((__unused_webpack_module, exports) => {
 
@@ -1772,7 +1772,7 @@ const log_rate_limit_break_1 = __nccwpck_require__(8956);
 const log_total_assessed_1 = __nccwpck_require__(2673);
 const log_total_deleted_1 = __nccwpck_require__(4888);
 const get_context_1 = __nccwpck_require__(7782);
-const filter_branches_1 = __nccwpck_require__(7168);
+const filter_branches_1 = __nccwpck_require__(6261);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         //Declare output arrays
