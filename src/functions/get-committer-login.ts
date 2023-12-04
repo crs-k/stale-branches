6f5642e930ada93feb81c62efaa23062e0a5b715
Linas Daneliukas <linas@daneliukas.eu>
2023-12-04 13:31:50 +0200

    Fallback to author.login if committer.login isn't available
    There are cases where the committer is empty, but the author is not. Use that when there's no committer info.

--- src/functions/get-committer-login.ts
+++ src/functions/get-committer-login.ts
@@ -19,7 +19,8 @@ export async function getRecentCommitLogin(sha: string): Promise<string> {
       per_page: 1,
       page: 1
     })
-    lastCommitter = commitResponse.data.committer!.login
+    const commitData = commitResponse.data;
+    lastCommitter = commitData.committer?.login || commitData.author!.login;
     assert.ok(lastCommitter, 'Committer cannot be empty.')
   } catch (err) {
     if (err instanceof Error) {
